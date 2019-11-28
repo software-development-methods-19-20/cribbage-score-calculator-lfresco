109895e900bf34159eb9c0d0bde6ebec2339fd19
lfresco <loryfresco@gmail.com>
2019-11-28 12:16:57 +0100

    commit
    implemented all methods and passed final test. still needs refactoring

--- production/source/dssc/Hand.java
+++ production/source/dssc/Hand.java
@@ -15,12 +15,28 @@ public class Hand {
         this.values = values;
         this.seeds = seeds;
     }
+
+    public static int parseLetters(char letter){
+        if(letter == 'A'){
+            return 1;
+        } else if(letter == '0'){
+            return 10;
+        } else if(letter == 'J'){
+            return 11;
+        } else if(letter == 'Q'){
+            return 12;
+        } else if(letter == 'K'){
+            return 13;
+        } else {
+            return Character.getNumericValue(letter);
+        }
+    }
     public static Hand parseHand(String input){
         ArrayList<Integer> values = new ArrayList<>();
         ArrayList<Character> seeds = new ArrayList<>();
 
         for(int i = 0; i < HANDSIZE; i = i + 2){
-            int value = Character.getNumericValue(input.charAt(i));
+            int value = parseLetters(input.charAt(i));
             char seed = input.charAt(i + 1);
 
             values.add(value);
--- test/source/test/cribbageTest.java
+++ test/source/test/cribbageTest.java
@@ -11,13 +11,13 @@ public class cribbageTest {
 
     @Test
     void checkPairs(){
-        assertThat(cribbageScore.score("5H5H8S6C7D"), is(2));
+        assertThat(cribbageScore.score("5H5H9S6CAD"), is(2));
 
     }
 
     @Test
     void checkRoayalPairs(){
-        assertThat(cribbageScore.score("5H5H5S6C7D"), is(6));
+        assertThat(cribbageScore.score("5H5H5S6C9D"), is(6));
     }
 
     @Test
@@ -29,4 +29,40 @@ public class cribbageTest {
     void checkMixOfPairs(){
         assertThat(cribbageScore.score("5H5H5S6C6D"), is(8));
     }
+
+    @Test
+    void runofThree(){
+        assertThat(cribbageScore.score("1H2H3S5C7D"), is(3));
+    }
+
+    @Test
+    void runofFour() { assertThat(cribbageScore.score("1H2H3S4C6D"), is(4));}
+
+    @Test
+    void runofFive() { assertThat(cribbageScore.score("1H2H3S4C5D"), is(5));}
+
+    @Test
+    void pairandrunofFour() {assertThat(cribbageScore.score("1H2H3S4C1D"), is(6));}
+
+    @Test
+    void checkLetters() {assertThat(cribbageScore.score("QHQHJSJC7D"), is( 4));}
+
+    @Test
+    void checkFlushofFour() {assertThat(cribbageScore.score("1H3H5H7H9D"), is(4));}
+
+    @Test
+    void chechkFlushofFive() {assertThat(cribbageScore.score("1H3H5H7H9H"), is(5));}
+
+    @Test
+    void checkFlushandJack() { assertThat(cribbageScore.score("AH3H6HJH9H"), is(5 + 1));}
+
+    @Test
+    void fifteenTwos() { assertThat(cribbageScore.score("0D1SH36D5H"), is(2));}
+
+    @Test
+    void finalTest() {
+        assertThat(cribbageScore.score("5H5D5SJC5C"), is(29));
+        assertThat(cribbageScore.score("0DJHQSAC9D"), is(4));
+    }
+
 }
